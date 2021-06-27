f7df1c56b7cef6eba7aa3ba81fb86c2d55c55943
patricksevilla <57100910+patricksevilla@users.noreply.github.com>
2021-06-27 19:06:07 +0800

    add the path and the data with the ball

--- p5.js compile/projectile_ninyaPam.js
+++ p5.js compile/projectile_ninyaPam.js
@@ -1,18 +1,38 @@
-// --------Projectile Motion-----------
 let xloc = 0;
 let yloc = 300;
 let xspeed = 10;
 let yspeed = -13;
 let acceleration = 9.81/30;
+let pathx = []
+let pathy = []
+
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  frameRate(5)
   background(220);
   displayProjectile();
   moveProjectile();
+  displayPath();
+  displayData()
+}
+
+function displayData(){
+  text("xloc: " + str(xloc),xloc + 5,yloc)
+  text("yloc: " + str(yloc),xloc + 5, yloc + 10)
+  text("yspeed: " + str(yspeed), xloc + 5, yloc + 20)
+}
+
+function displayPath(){
+  pathx.push(xloc)
+  pathy.push(yloc)
+  
+  for(let i = 0; i < pathx.length; i++){
+    point(pathx[i],pathy[i])
+  }
 }
 
 function displayProjectile() {
@@ -24,5 +44,3 @@ function moveProjectile() {
   yloc = yloc + yspeed;
   yspeed = yspeed +acceleration
 }
-
-// -----------i-uupdate pa po itu--------------
