d5047b66b5723ffbe706abaf95b6103f9106db47
nenyadlrsr <85609367+nenyadlrsr@users.noreply.github.com>
2021-06-27 18:35:54 +0800

    Projectile Motion
    *Javascript code for Projectile Motion (Visual)
    *will be updated...soon hehe

--- p5.js
+++ p5.js
@@ -55,3 +55,33 @@ function showrelativemotion(){
   carB_yvelo = carA_yvelo - carB_yvelo;
   carA_yvelo *= 0
 }//di ko na kaya antok2
+
+
+--------Projectile Motion-----------
+let xloc = 0;
+let yloc = 300;
+let xspeed = 10;
+let yspeed = -13;
+let acceleration = 9.81/30;
+
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(220);
+  displayProjectile();
+  moveProjectile();
+}
+
+function displayProjectile() {
+  ellipse(xloc, yloc, 20, 20);
+}
+
+function moveProjectile() {
+  xloc = xloc + xspeed;
+  yloc = yloc + yspeed;
+  yspeed = yspeed +acceleration
+}
+
+-----------i-uupdate pa po itu--------------
