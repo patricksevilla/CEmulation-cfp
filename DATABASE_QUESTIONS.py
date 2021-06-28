dca62999797bda51a49f98a5e1c202c197e206a5
RuzchelEstrella <85602337+RuzchelEstrella@users.noreply.github.com>
2021-06-28 14:31:58 +0800

    Add files via upload
    DATABASE QUESTIONS (KINEMATICS)

---
+++ DATABASE_QUESTIONS.py
@@ -0,0 +1,454 @@
+# coding: utf-8
+question_list = [['A laser beam is aimed 15.95° above the horizontal at a mirror 11,648 m away. It glances off the '
+     'mirror and continues for an additional 8570. m at 11.44° above the horizon until it hits its target. What is '
+     'the resultant displacement of the beam to the target?',
+     ['20, 203 m , 14.04 °',
+      '9, 549 m , 13.04 °',
+      '20, 103 m , 14.14 °',
+      '10, 203 m , 14.04 °'],
+      '20, 203 m , 14.04 °'],
+[#Question
+     'Vector A represents the first leg of a walk in which a person walks 53.0 m in a direction 20.0º north of east. '
+     'Vector B represents the second leg, a displacement of 34.0 m in a direction 63.0º north of east. Find the '
+     'magnitude and direction of the resultant.',
+     [#Choices
+      '80.2 m , 35. 7 °',
+      '76.4 m , 26. 5 °',
+      '81.2 m , 36.6 °',
+      '134.6 m , 26.6 °'],
+     #Answer
+      '81.2 m , 36.6 °'],
+[#Question
+     'A person is located 225 m and 35.0° north of west from her initial position. She walked from her initial '
+     'position to her final position in two stages. In the first stage of her walk, she walked 150 m at 20.0° south '
+     'of east. How far and in what direction did she walk in the second part?',
+     [#Choices
+      '172 m , 19.0°',
+      '256 m , 10.1 °',
+      '460 m , 31.5 °',
+      '372 m , 29.0°'],
+     #Answer
+      '372 m , 29.0°'],
+[#Question
+     'A bus covers a certain distance at an average speed of 40 mph, while returning the same distance it runs at an '
+     'average speed of 60 mph. What is the average speed of the bus throughout the journey?',
+     [#Choices
+      '48 mph',
+      '50 mph',
+      '52 mph',
+      '54 mph'],
+     #Answer
+      '48 mph'],
+[#Question
+     'On a certain day, Paul started driving towards Steve’s place at a speed of 45 mph. After moving for 2 hours, he '
+     'realized that he needed to reach Steve’s place in another 2 hours only. Therefore, he increased his speed to 65 '
+     'mph and reached Steve’s place exactly on time. What is the average speed of his journey?',
+     [#Choices
+      '48 mph',
+      '50 mph',
+      '52 mph',
+      '54 mph'],
+     #Answer
+      '48 mph'],
+[#Question
+     'If the average speed of bus during the entire journey is 16 km/hr, and if in the first half of the distance the '
+     'average speed of the bus was 10 km/hr then what was the average speed for the second half of trip?',
+     [#Choices
+      '10 km/hr',
+      '16 km/hr',
+      '20 km/hr',
+      '40 km/hr'],
+     #Answer
+      '40 km/hr'],
+[#Question
+     'If magnitude of average speed and average velocity over a time interval are same, then',
+     [#Choices
+      'The particle must move zero acceleration',
+      'The particle must move with non-zero acceleration',
+      'The particle must be at rest',
+      'The particle must move in a straight line without turning back'],
+     #Answer
+      'The particle must move in a straight line without turning back'],
+[#Question
+     'A roller coaster car rapidly picks up speed as it rolls down a slope. As it starts down the slope, its speed is '
+     '4 m/s. But 3 seconds later, at the bottom of the slope, its speed is 22 m/s. What is its average acceleration?',
+     [#Choices
+      '5 m/s^2',
+      '6 m/s^2',
+      '9 m/s^2',
+      '12 m/s^2'],
+     #Answer
+      '6 m/s^2'],
+[#Question
+     'The position (in meters) of a particle moving in a straight line is given by s(t)=4t^3+6t+2,s(t)=4t3+6t+2, where '
+     't is measured in seconds. What is the instantaneous velocity of the particle at t=8t=8 seconds? ',
+     [#Choices
+      '777 m/s',
+      '776 m/s',
+      '774 m/s',
+      '775 m/s'],
+     #Answer
+      '774 m/s'],
+[#Question
+     'The motion of the car is provided by the function x = 4t2 + 10t + 6. Compute its Instantaneous Velocity at time '
+     't = 5s. ',
+     [#Choices
+      '50 m/s',
+      '53 m/s',
+      '54 m/s',
+      '57 m/s'],
+     #Answer
+      '50 m/s'],
+[#Question
+     'A bullet fired in space is traveling in a straight line and its equation of motion is S(t) = 4t + 6t2. If it '
+     'travels for 15 seconds before impact, find the instantaneous velocity at the 10th second. ',
+     [#Choices
+      '124m/s',
+      '121m/s',
+      '126m/s',
+      '123m/s'],
+     #Answer
+      '124m/s'],
+[#Question
+     'A particle is in motion and is accelerating. The functional form of the velocity is v(t) = 20t − 5t2 m/s. Find '
+     'the instantaneous acceleration at t = 1, 2, 3, and 5 s. ',
+     [#Choices
+      '30 m/s^2',
+      '-30 m/s^2',
+      '40 m/s^2',
+      '-40 m/s^2'],
+     #Answer
+      '-30 m/s^2'],
+[#Question
+     'Consider a man travelling at a velocity of 40 m/s. He then accelerates by 4 m/s^2 for 10 seconds. Find the final '
+     'velocity of the man. ',
+     [#Choices
+      '70 m/s',
+      '80 m/s',
+      '90 m/s',
+      '100 m/s'],
+     #Answer
+      '80 m/s'],
+[#Question
+     'A girl left her house on her bicycle with a speed of 6 m/s. She decided to speed up to catch her friend she saw '
+     'nearby. She accelerates by 2 m/s for 5 seconds to be able to do that. What is the distance of the place where '
+     'she will meet her friend from their house?',
+     [#Choices
+      '50 m',
+      '52 m',
+      '55 m',
+      '58 m'],
+     #Answer
+      '55 m'],
+[#Question
+     'For a goal distance of 100 meters, a boy wants to run with a speed of 10 m/s and acceleration of 2 m/s to '
+     'improve his endurance. Find his final velocity when he finished his goal distance.',
+     [#Choices
+      '22.33 m/s',
+      '22.34 m/s',
+      '22.35 m/s',
+      '22.36 m/s'],
+     #Answer
+      '22.36 m/s'],
+[#Question
+     'What do you call the motion of an object due solely to the gravitational pull of the Earth over small distances?',
+     [#Choices
+      'Projectile Motion',
+      'Free Fall',
+      'Horizontal Motion',
+      'Relative Motion'],
+     #Answer
+      'Free Fall'],
+[#Question
+     'A man standing at a window 5m tall watches a falling ball pass by the window in 0.3 seconds. From how high above '
+     'the top of the window was the ball released?',
+     [#Choices
+      '15.20 m',
+      '13.68 m',
+      '11.78 m',
+      '10.00 m'],
+     #Answer
+      '11.78 m'],
+[#Question
+     'The following statement demonstrate free fall EXCEPT: ',
+     [#Choices
+      'Skydiving',
+      'A stone thrown downward',
+      'A spacecraft in continuous orbit',
+      'Parachuting'],
+     #Answer
+      'Parachuting'],
+[#Question
+     'Object A and object B were thrown downward from a h tall building simultaneously with the same initial velocity. '
+     'If the mass of object A is twice the mass of object B, which will reach the ground first? Note that air'
+     'resistance is neglected.',
+     [#Choices
+      'Object A will land first because its mass is twice the mass of object B.',
+      'Object B will land first because its mass is half the mass of object A.',
+      'Object A and B will land at the same time.',
+      'Object A and B will never land.'],
+     #Answer
+      'Object A and B will land at the same time.'],
+[#Question
+     'What is the force that attracts a body toward the center of the earth? ',
+     [#Choices
+      'Centripetal Force',
+      'Gravity',
+      'Centrifugal Force',
+      'Velocity'],
+     #Answer
+      'Gravity'],
+[#Question
+     'Craving for a guava, a boy decided to climb up the guava tree to pick some. Up there, he saw a yellowish guava '
+     'which he immediately picked to satisfy his craving. The moment he had it in his hands, he noticed that there is '
+     'a worm feeding on it. What he did is throw the guava fruit up in the air with an initial velocity of 12 m/s at a '
+     '15-degree angle of release. The time of flight of the guava is estimated to be 1.47 s. At what height did the '
+     'boy throw it?',
+     [#Choices
+      '6.0 m',
+      '6.1 m',
+      '6.2 m',
+      '6.3 m'],
+     #Answer
+      '6.3 m'],
+[#Question
+     'In a basketball game, Kyle released the ball 2.2m above the ground when he was 8.2m away horizontally from the '
+     'basket. The basket is 3m above the ground. After 2 seconds, the ball sank in the basket. Find the initial '
+     'velocity when the angle of release is 35 degrees. ',
+     [#Choices
+      '10.00 m/s',
+      '14.30 m/s',
+      '10.01 m/s',
+      '14.29 m/s'],
+     #Answer
+      '10.01 m/s'],
+[#Question
+     'Since it is summer, Ella wants to try an extreme experience in their outing. As they arrived to the location of '
+     'their swimming, she immediately climb a 50 m high pool diving board and jumped with an initial velocity of 9.6 '
+     'm/s and a 23 degree angle. Within 3.71 s she reached the pool. How far is she from the starting point? ',
+     [#Choices
+      '35.62 m',
+      '32.78 m',
+      '13.92 m',
+      '12.54 m'],
+     #Answer
+      '32.78 m'],
+[#Question
+     'An object is launched at a velocity of 20 m/s in a direction making an angle of 25° upward with the horizontal. '
+     'What is the maximum height reached by the object? ',
+     [#Choices
+      '6.59 m',
+      '4.64 m',
+      '3.64 m',
+      '3.18 m'],
+     #Answer
+      '3.64 m'],
+[#Question
+     'An object is launched at a velocity of 20 m/s in a direction making an angle of 25° upward with the horizontal. '
+     'What is the total flight time (between launch and touching the ground) of the object? ',
+     [#Choices
+      '0.86 s',
+      '1.72 s',
+      '3.97 s',
+      '2.55 s'],
+     #Answer
+      '1.72 s'],
+[#Question
+     'An object is launched at a velocity of 20 m/s in a direction making an angle of 25° upward with the horizontal. '
+     'What is the horizontal range (maximum x above ground) of the object? ',
+     [#Choices
+      '33.33 m',
+      '31.26 m',
+      '30.04 m',
+      '28.57 m'],
+     #Answer
+      '31.26 m'],
+[#Question
+     'An object is launched at a velocity of 20 m/s in a direction making an angle of 25° upward with the horizontal. '
+     'What is the magnitude of the velocity of the object just before it hits the ground? ',
+     [#Choices
+      '10 m/s',
+      '30 m/s',
+      '40 m/s',
+      '20 m/s'],
+     #Answer
+      '20 m/s'],
+[#Question
+     'A 0.2 kg ball rotates at a constant speed of 3 m/s on the end of 1.2 m long string. What is the centripetal '
+     'acceleration of the object? ',
+     [#Choices
+      '1.2 m/s^2',
+      '3.0 m/s^2',
+      '7.5 m/s^2',
+      '3.2 m/s^2'],
+     #Answer
+      '7.5 m/s^2'],
+[#Question
+     'A child sits and accelerates with 19 m/s^2 at a distance of 3.0 m from the axis of a merry-go-round in an '
+     'amusement park. Find its tangential speed. ',
+     [#Choices
+      '8.34 m/s',
+      '7.54 m/s',
+      '6.24 m/s',
+      '8.04 m/s'],
+     #Answer
+      '7.54 m/s'],
+[#Question
+     'The earth orbits the sun at a distance of 1.5 x 10^11 m. What is the centripetal acceleration of the earth '
+     'around the sun? ',
+     [#Choices
+      '5.95 x 10^-3 m/s^2',
+      '4.35 x 10^4 m/s^2',
+      '5.95 x 10^3 m/s^2',
+      '4.35 x 10^-4 m/s^2'],
+     #Answer
+      '5.95 x 10^-3 m/s^2'],
+[#Question
+     'In a carnival ride , the passengers travel in a circle of radius 5.0 m, making one complete circle in 4.0 s. '
+     'What is the speed? ',
+     [#Choices
+      '7.85 m/s',
+      '8.75 m/s',
+      '5.78 m/s',
+      '6.57 m/s'],
+     #Answer
+      '7.85 m/s'],
+[#Question
+     'A toy car is held in a circle by a 0.10 m string attached to it when allowed to run in a flat track while the ca'
+     'r is exerting a constant speed of 3.25 m/s. How much time needed for an object to make one complete revolution? ',
+     [#Choices
+      '0.4 s',
+      '0.3 s',
+      '0.2 s',
+      '0.5 s'],
+     #Answer
+      '0.2 s'],
+[#Question
+     'A small ant is sleeping on a turntable just as the turntable starts to spin from rest, with an angular '
+     'acceleration α=1rad/s2α=1rad/s2 that is small enough so that, initially, the ant remains on the turntable. The '
+     'ant is a distance R=0.1mR=0.1m from the center of the turntable, as shown in Figure 6.4.16.4.1 and the '
+     'coefficient of static friction between the ant’s “feet” and the turntable is μs=0.5μs=0.5. After how much time '
+     'will the ant slide off from the turntable?',
+     [#Choices
+      '2s',
+      '4.5s',
+      '7s',
+      '6s'],
+     #Answer
+      '7s'],
+[#Question
+     'A 1200kg car accelerates from rest to speed of 40 m/s in 5 seconds at a constant rate on a circular track of '
+     'radius 800m. What is the tangential acceleration?',
+     [#Choices
+      '9m/s^2',
+      '12m/s^2',
+      '8m/s^2',
+      '0m/s^2'],
+     #Answer
+      '8m/s^2'],
+[#Question
+     'The net acceleration is no longer pointing towards the centre of the circle',
+     [#Choices
+      'True',
+      'False',
+      'Partly True',
+      'Partly False'],
+     #Answer
+      'True'],
+[#Question
+     'There are 2 components of acceleration: radial and centripetal.',
+     [#Choices
+      'True',
+      'False',
+      'Partly True',
+      'Partly False'],
+     #Answer
+      'False'],
+[#Question
+     'A car moving on a crowded road with a number of traffic red signals is in non-uniform motion.',
+     [#Choices
+      'True',
+      'False',
+      'Partly True',
+      'Partly False'],
+     #Answer
+      'True'],
+[#Question
+     'Displacement of a body can be positive or zero, but never negative.',
+     [#Choices
+      'True',
+      'False',
+      'Partly True',
+      'Partly False'],
+     #Answer
+      'False'],
+[#Question
+     'Angular displacement is measured in radians.',
+     [#Choices
+      'True',
+      'False',
+      'Partly True',
+      'Partly False'],
+     #Answer
+      'True'],
+[#Question
+     'A freely falling body is uniformly accelerated.',
+     [#Choices
+      'True',
+      'False',
+      'Partly True',
+      'Partly False'],
+     #Answer
+      'True'],
+[#Question
+     'You drive north on a straight two-lane road at a constant 88 km/h. A truck in the other lane approaches you '
+     'at a constant 104 km/h. Find the truck’s velocity relative to you.',
+     [#Choices
+      '192 km/h',
+      '-192 km/h',
+      '16 km/h',
+      '-16 km/h'],
+     #Answer
+      '-192 km/h'],
+[#Question
+     'A plane’s compass indicates that it is headed due north, and its airspeed indicator shows that it is moving '
+     'through the air at 240 km/h. If there is a 100- km/h wind from west to east, what is the velocity of the plane '
+     'relative to the earth?',
+     [#Choices
+      '260 km/h',
+      '340 km/h',
+      '140 km/h',
+      '170 km/h'],
+     #Answer
+      '260 km/h'],
+[#Question
+     'A motorcycle traveling on the highway at a speed of 120 km/h passes a car traveling at a speed of 90 km/h. From '
+     'the point of view of a passenger on the car, what is the velocity of the motorcycle? ',
+     [#Choices
+      '210 km/h',
+      '-210 km/h',
+      '30 km/h',
+      '-30 km/h'],
+     #Answer
+      '30 km/h'],
+[#Question
+     'A ship sails North in still water at 25 m/s directly across the sea that runs East at 11 m/s. What is the '
+     'velocity of the ship relative to Earth?',
+     [#Choices
+      '36 m/s',
+      '27.31 m/s',
+      '14 m/s',
+      '-36 m/s'],
+     #Answer
+      '27.31 m/s'],
+[#Question
+     'Across the river that flows with a rate of 4 m/s, Mr. Pepper swims at the velocity of 3 m/s. Find his velocity '
+     'relative to the Earth.',
+     [#Choices
+      '1 m/s',
+      '-1 m/s',
+      '7 m/s',
+      '5 m/s'],
+     #Answer
+      '5 m/s'],
+]
