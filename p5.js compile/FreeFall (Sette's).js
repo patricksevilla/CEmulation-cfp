3667c4f8b690421a9dec8d9fb2195ad51aaca6c5
settemeneses <85600170+settemeneses@users.noreply.github.com>
2021-07-03 15:01:01 +0800

    Updated Free Fall Simulator

--- p5.js compile/FreeFall (Sette's).js
+++ p5.js compile/FreeFall (Sette's).js
@@ -1,9 +1,9 @@
-let ylocation = 380
-let velocity = 5
-let initvelo = 5
+let ylocation = 500
+let velocity = 7
+let initvelo = 7
 let newVelocity = 0
 let g = -9.81/30
-let showVelocity = '5 m/s'
+let showVelocity = '7 m/s'
 var vid;
 let vidRatio = 1920/1080
 let vidx, vidy, vidWidth, vidHeight;
@@ -31,11 +31,11 @@ function setup() {
   vid.loop();
   
   button = createButton('PLAY');
-  button.position(vidx+30 ,238);
+  button.position(width*0.044,height*0.48);
   button.mousePressed(ChangeValues);
   
-  newVelocity = createSlider(0,15,5);
-  newVelocity.position (vidx+145,213);
+  newVelocity = createSlider(0,17,7);
+  newVelocity.position (width*0.158,height*0.437);
   newVelocity.size(100);
   newVelocity.changed(ChangeValues);
   newVelocity.mouseOver(ShowVelocityChoice);
@@ -59,8 +59,8 @@ function ChangeValues()
 { 
   if (isNaN(newVelocity.value()))
     {
-      velocity = 5;
-      initvelo = 5;
+      velocity = 7;
+      initvelo = 7;
     }
   else
     {
@@ -75,15 +75,15 @@ function lining()
   if (velocity >=0)
   {
     a = ylocation;
-    line (width*0.56, 380, width*0.56, ylocation);
+    line (width*0.56, 500, width*0.56, ylocation);
   }
   else
     {
-      line (width*0.56, 380, width*0.56, a);
+      line (width*0.56, 500, width*0.56, a);
       line (width*0.56 - 20, a, width*0.56 + 20, a);
-      maxH = 'Maximum Height: ' + round(((380-a)/30),2) + ' m';
+      maxH = 'Maximum Height: ' + round(((500-a)/30),2) + ' m';
       textSize(15);
-      text(maxH, width * 0.56 + 25, a+3);
+      text(maxH, width * 0.56 +10, a+15);
     }
 }
 
@@ -92,48 +92,48 @@ function FreeBodyDiagram()
   if (velocity >=0)
   {
     z = ylocation;
-    line (width * 0.78, 380, width * 0.78, ylocation);
+    line (width * 0.80, 500, width * 0.80, ylocation);
   }
   else
     {
       textSize(15)
-      text('Free Body Diagram', width * 0.72, a-5)
-      line (width * 0.78, 380, width * 0.78, (z+2));
-      line (width * 0.78, (z+2), width * 0.79, (z-1));
-      line (width * 0.79, (z-1), width * 0.8, (z+2));
-      line (width * 0.8, (z+2), width * 0.8, 380);
-      line (width * 0.798, 375, width * 0.8, 380);
-      line (width * 0.802, 375, width * 0.8, 380)
+      text('Free Body Diagram', width * 0.75, a-5)
+      line (width * 0.80, 500, width * 0.80, (z+2));
+      line (width * 0.80, (z+2), width * 0.81, (z-1));
+      line (width * 0.81, (z-1), width * 0.82, (z+2));
+      line (width * 0.82, (z+2), width * 0.82, 500);
+      line (width * 0.82 -2, 495, width * 0.82, 500);
+      line (width * 0.82 +2, 495, width * 0.82, 500)
     }
 }
 
 function displayHeights()
 {
   b = mouseY;
-  h = 'Height: ' + ((380-b)/30) + ' m';
+  h = round(((500-b)/30), 4) + ' m';
   textSize(15);
-  text(h, 500, 150);
+  text(h, width*0.1021, height*0.8759 -9);
 }
 
 function displaySpeedUpward()
 {
   b = mouseY;
-  v = Math.sqrt((initvelo ** 2) + (2*g*(380-b)));
+  v = Math.sqrt((initvelo ** 2) + (2*g*(500-b)));
   if (isNaN(v))
     {
       v = 0;
     }
-  vPos = 'Velocity: ' + v + ' m/s';
+  vPos = round(v, 4) + ' m/s';
   textSize(15);
-  text(vPos, 500, 170);
+  text(vPos, width*0.1073 + 2, height*0.8759 + 11);
 }
 
 function displayTimeUpward()
 {
   t = ((v-initvelo)/g);
-  tUpward = 'Time: ' + (t/30)+' s';
+  tUpward = round((t/30), 4)+' s';
   textSize(15);
-  text(tUpward, 500, 190);
+  text(tUpward, width*0.1021 -13, height*0.8759 +30);
 }
 
 function displayDetailsUpward()
@@ -146,29 +146,29 @@ function displayDetailsUpward()
 function displaySpeedDownward()
 {
   b = mouseY;
-  vd = Math.sqrt((initvelo ** 2) + (2*g*(380-b)));
+  vd = Math.sqrt((initvelo ** 2) + (2*g*(500-b)));
   if (isNaN(vd))
     {
       vd = 0;
     }
-  vNeg = 'Velocity: ' + (vd*(-1)) + ' m/s';
+  vNeg = round((vd*(-1)), 4) + ' m/s';
   textSize(15);
-  text(vNeg, 500, 170);
+  text(vNeg, width*0.1073 + 2, height*0.8759 + 11);
 }
 
 function displayTimeDownward()
 {
   tAtMax = ((initvelo/g)*(-1));
   b = mouseY;
-  v = Math.sqrt((initvelo ** 2) + (2*g*(380-b)));
+  v = Math.sqrt((initvelo ** 2) + (2*g*(500-b)));
   if (isNaN(v))
     {
       v = 0;
     }
   t = tAtMax - ((v-initvelo)/g);
-  tDownward = 'Time: ' + ((tAtMax/30)+(t/30)) + ' s';
+  tDownward = round(((tAtMax/30)+(t/30)), 4) + ' s';
   textSize(15);
-  text(tDownward, 500, 190);
+  text(tDownward, width*0.1021 -13, height*0.8759 +30);
 }
 
 function displayDetailsDownward()
@@ -181,15 +181,13 @@ function displayDetailsDownward()
 function draw() 
 {
   image(vid, vidx, vidy, vidWidth, vidHeight);
-  textSize(15);
-  text(showVelocity, vidx+148, 245);
   
   if (velocity >= 0)
     {
       ylocation = ylocation - velocity;
     }
   
-  else if (velocity <= 0 && ylocation <= 380)
+  else if (velocity <= 0 && ylocation <= 500)
     {
       ylocation = ylocation - velocity;
     }
@@ -202,23 +200,26 @@ function draw()
   circle(width*0.56,ylocation, 20);
   FreeBodyDiagram();
   
-  if (mouseX >= 777 && mouseX <= 783 && mouseY <= 380 && mouseY >= a)
+  fill(220);
+  textFont('Comic Sans MS');
+  textSize(15);
+  text(showVelocity, width*0.158+3, height*0.4925);
+  
+  if (mouseX >= (width*0.80 - 3) && mouseX <= (width*0.80 + 3) && mouseY <= 500 && mouseY >= a)
   {
     displayDetailsUpward();
     fill (65, 105, 225)
   }
   else
   {
-    if(mouseX >= 797 && mouseX <= 803 && mouseY <= 380 && mouseY >= a)
+    if(mouseX >= (width*0.82 - 3) && mouseX <= (width*0.82 + 3) && mouseY <= 500 && mouseY >= a)
   {
     displayDetailsDownward();
     fill (255, 99, 71)
   }
   else
     {
-      fill(128, 128, 0)
+      fill(0)
     }
   }
 }
-
-  
