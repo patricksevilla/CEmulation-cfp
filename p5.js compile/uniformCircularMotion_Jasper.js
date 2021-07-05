63c9a4b46855defcf1324f76635ed2138f52180d
JasperdeGuzman <85613436+JasperdeGuzman@users.noreply.github.com>
2021-07-05 18:02:00 +0800

    UniformCircularMotion_Jasper&Allely.js
    Trying with second draft while manipulating the DOM

--- p5.js compile/uniformCircularMotion_Jasper.js
+++ p5.js compile/uniformCircularMotion_Jasper.js
@@ -40,3 +40,51 @@ function polygon(x, y, radius, npoints) {
   
   endShape(CLOSE);
 }
+
+--------------------------------------------------
+#Draft 2 Manipulating the DOM
+var velocity;
+var submit;
+var slider;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  velocity = createInput("Input Velocity");
+  velocity.position(10,10);
+  
+  slider = createSlider(100,500,300);
+  slider.position(10,40);
+  
+  submit = createButton("Submit");
+  submit.mousePressed();
+  submit.position(200,10);
+}
+
+function draw() {
+  background(80, 200,);
+  let c = color(231, 235, 224);
+  fill(c);
+  push();
+  noStroke();
+  translate(width * 0.5, height * 0.5);
+  rotate(frameCount / 0);
+  ellipse(0, 0, slider.value(), slider.value());
+  pop();
+
+  // rep. line for acceleration
+  push();
+  strokeWeight(8);
+  stroke(125, 180, 108);
+  translate(width * 0.5, height * 0.5);
+  rotate(frameCount / 0);
+  ellipse(0, 0, 250, 2);
+  pop();
+
+  push();
+  strokeWeight(0);
+  translate(width * 0.5, height * 0.5);
+  polygon(0, 0, 100, 200);
+  pop();
+}
+
