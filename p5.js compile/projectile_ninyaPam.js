49d03d18e7640d4c81380e6e1475b08f6554b3db
patricksevilla <57100910+patricksevilla@users.noreply.github.com>
2021-07-07 15:18:23 +0800

    Update projectile_ninyaPam.js

--- p5.js compile/projectile_ninyaPam.js
+++ p5.js compile/projectile_ninyaPam.js
@@ -1,29 +1,58 @@
 let xloc = 55;
 let yloc = 290;
 let x = 0
-let y = 260
+let y;
 let xspeed = 10;
 let yspeed = -13;
 let acceleration = 9.81 / 30;
 let pathx = [];
 let pathy = [];
+let state = "not moving";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  
   img1 = loadImage('bbg.jpg')
   img2 = loadImage('CANNONBALL.png')
   img3 = loadImage('CANNON.png')
+  
+  slider = createSlider(0,200,0)
+  slider.position(0,0)
+  
+  input1 = createInput()
+  submit = createButton("submit")
+  submit.mousePressed(go)
+  
+  input2 = createInput()
+  
 }
 
 function draw() {
   frameRate(5);
   background(220);
   image(img1, 0, 0, windowWidth, windowHeight);
+  
+  
+  if(state == "not moving"){
+    xspeed = 0
+    yspeed = 0
+    xloc = 80
+    yloc = height * 0.72121212 - slider.value() - 52
+    
+  }else if(state == "moving"){
+    xspeed = parseInt(input1.value())
+    yspeed = parseInt(input2.value()) * -1
+  }
+  
+
+  
   displayProjectile();
   moveProjectile();
   displayPath();
   displayData();
 }
+
 
 function displayData() {
   text("xloc: " + str(xloc), xloc + 5, yloc);
@@ -41,12 +70,23 @@ function displayPath() {
 }
 
 function displayProjectile() {
-  image(img2, xloc, yloc, 20, 20);
+  image(img2, xloc - 10, yloc - 10, 20, 20);
+  
+  y = height * 0.72121212 - 80 - slider.value()
   image(img3, x, y, 80, 80)
 }
 
 function moveProjectile() {
   xloc = xloc + xspeed;
   yloc = yloc + yspeed;
-  yspeed = yspeed + acceleration;
+}
+
+function go(){
+  if(state == "not moving"){
+    state = "moving"
+    submit.html("reset")
+  }else if(state == "moving"){
+    state = "not moving"
+    submit.html("submit")
+  }
 }
