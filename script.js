944f590603a3b708aedff94c0088022c184b4706
RuzchelEstrella <85602337+RuzchelEstrella@users.noreply.github.com>
2021-07-16 18:05:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ resetButton.addEventListener('click', () =>{
     location.reload();
     })
 backButton.addEventListener('click', () => {
-    location.assign('https://patricksevilla.github.io/CFP-Project/CE-1D/?fbclid=IwAR2oItDJjoSzH0YdPydkl5WYdzWgeXkXknc4ATjzNK6GViggB442g85yyl4')//pakipalitan na lang ito ng url or link nung intro page na aking pinapagawa
+    location.assign('https://patricksevilla.github.io/CEmulation/CE-1D/?fbclid=IwAR06tUO4BX22eE_z-udSPdy54DbibYZtH6yYWBCl1jxhKCT_zmSMER1CsP8')//pakipalitan na lang ito ng url or link nung intro page na aking pinapagawa
 })
 
 function startGame() {
